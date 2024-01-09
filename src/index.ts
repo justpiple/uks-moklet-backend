9bb3e74e560fd414d4fdfd438aadb62f121c19fc
FadhilKholaf <fadhilgd123@gmail.com>
2024-01-09 13:45:17 +0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+// app.ts
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
-const server = app.listen(3000, () =>
-  console.log(`🚀 Server ready at: http://localhost:3000`)
-);
+// Route to create a new siswa
+app.post("/siswa", async (req, res) => {
+  try {
+    const { email, name, gender, password, register, rombel } = req.body;
+
+    const createdSiswa = await prisma.siswa.create({
+      data: {
+        email,
+        name,
+        gender,
+        password,
+      },
+    });
+
+    res.json(createdSiswa);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
