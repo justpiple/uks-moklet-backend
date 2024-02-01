d8db23e3ad518f097ae1341f85a041b5f40880f7
FadhilKholaf <fadhilgd123@gmail.com>
2024-02-01 13:22:56 +0700

    feat: create CRUD system for siswa

---
+++ src/controllers/siswa/siswa.controller.ts
@@ -0,0 +1,82 @@
+import {
+  findSiswaById,
+  createSiswa,
+  updateSiswa,
+  deleteSiswa,
+} from "@/utils/queries/siswa.query";
+import { Prisma } from "@prisma/client";
+import { Request, Response } from "express";
+import { Success, BadRequest, CreatedSuccessfully } from "@/utils/apiResponse";
+import { uuidv7 } from "uuidv7";
+
+interface siswaReqProps extends Request {
+  body: Prisma.SiswaUncheckedCreateInput;
+}
+
+// FIND SISWA BY ID
+export const FindSiswaById = async (req: Request, res: Response) => {
+  try {
+    const response = await findSiswaById(req.params.id);
+    if (response == null) {
+      return res.status(400).json(BadRequest("Cannot find any siswa"));
+    }
+    return res
+      .status(200)
+      .json(Success("Siswa loaded successfully", response));
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(BadRequest(JSON.stringify(error)));
+  }
+};
+
+// CREATE NEW SISWA
+export const CreateSiswa = async (req: siswaReqProps, res: Response) => {
+  try {
+    const data: Prisma.SiswaUncheckedCreateInput = {
+      ...req.body,
+      id: uuidv7(),
+    };
+    const response = await createSiswa(data);
+    if (!response) {
+      return res.status(400).json(BadRequest("Failed creating siswa"));
+    }
+    return res
+      .status(200)
+      .json(CreatedSuccessfully("Siswa created successfully", response));
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(BadRequest(JSON.stringify(error)));
+  }
+};
+
+// UPDATE EXISTING SISWA
+export const UpdateSiswa = async (req: siswaReqProps, res: Response) => {
+  try {
+    const response = await updateSiswa(req.params.id, req.body);
+    if (!response) {
+      return res.status(400).json(BadRequest("Failed updating siswa"));
+    }
+    return res
+      .status(200)
+      .json(Success("Siswa updated successfully", response));
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(BadRequest(JSON.stringify(error)));
+  }
+};
+
+// DELETE SISWA
+export const DeleteSiswa = async (req: Request, res: Response) => {
+  try {
+    const response = await deleteSiswa(req.params.id);
+    if (!response) {
+      return res.status(400).json(BadRequest("Cannot find any siswa"));
+    }
+    return res
+      .status(200)
+      .json(Success("Siswa deleted successfully", response));
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(BadRequest(JSON.stringify(error)));
+  }
+};
